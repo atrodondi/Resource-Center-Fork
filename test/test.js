b309ff503e3d22e43010fa3ff814d5948616d101
cheebachong <atrodondi@gmail.com>
2020-12-31 14:09:11 -0800

    added a confirmation of user registration to reg UI test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
-const { Builder, By } = require("selenium-webdriver");
+const { Builder, By, until } = require("selenium-webdriver");
+const assert = require("assert");
 
 let date = new Date();
+let time = date.getTime();
 
 describe("Register New User", () => {
-  it("should register a new user to the app, and bring user to dashboard/homescreen", async () => {
+  it("should register a new user to the app, and bring user to dashboard/homescreen where they see a welcome message", async () => {
     let driver = await new Builder().forBrowser("chrome").build();
+
     try {
       // try to log in and act as a registering user
       await driver.get("http://localhost:3000/");
@@ -14,7 +17,7 @@ describe("Register New User", () => {
       // sending keys to the new email input field
       await driver
         .findElement(By.name("regEmail"))
-        .sendKeys("newUser" + date + "@test.com");
+        .sendKeys("newUser" + time + "@gmail.com");
       // sending keys to the new username field
       await driver
         .findElement(By.name("regUsername"))
@@ -25,7 +28,11 @@ describe("Register New User", () => {
         .sendKeys("password" + date);
       //hitting submit button to complete the form
       await (await driver.findElement(By.className("btn-primary"))).click();
-      // here should probably add finding the welcome message on the dashboard to confirm that they are on the dashboard, this just confirms they the button was clicked.
+
+      //wait for the submit button pusb to register and grab the welcome message that matches the new registered user, confirming that user registration has been successful
+      await driver.wait(until.elementLocated(By.css("h2.welcoming")), 10000);
+      let text = await (await driver.findElement(By.css("h2"))).getText();
+      assert(text, "Welcome, newUsername" + date);
     } finally {
       await driver.quit();
     }
